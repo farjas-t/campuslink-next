b0381286484147d1639e24451bb69227e0366cae
Farjas T <farjasktr@gmail.com>
2024-01-19 23:22:03 +0530

    admin create student

--- src/app/admin/dashboard/student/page.tsx
+++ src/app/admin/dashboard/student/page.tsx
@@ -59,7 +59,7 @@ export default async function page({ searchParams }: paramsProps) {
           />
 
           <Link
-            href={"/dashboard/student/new"}
+            href={"/admin/dashboard/student/new"}
             className={cn(buttonVariants({ variant: "default" }))}
           >
             <Plus className="mr-2 h-4 w-4" /> Add New
