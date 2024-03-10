1bca052fcf4c55397f9515887510068d647b2b64
Farjas T <farjasktr@gmail.com>
2024-03-10 21:35:08 +0530

    teacher timetable add

---
+++ src/app/teacher/dashboard/timetable/[SemId]/page.tsx
@@ -0,0 +1,175 @@
+"use client";
+import {
+  JSXElementConstructor,
+  Key,
+  PromiseLikeOfReactNode,
+  ReactElement,
+  ReactNode,
+  ReactPortal,
+  useEffect,
+  useState,
+} from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
+async function fetchPapers(semId: string) {
+  const response = await fetch(`http://localhost:3500/paper/semester/${semId}`);
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error("Failed to fetch papers");
+}
+
+async function fetchTimetable(semId: string) {
+  const response = await fetch(`http://localhost:3500/time_schedule/${semId}`);
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error("Failed to fetch timetable");
+}
+
+async function saveTimetable(semId: string, timetable: any) {
+  const method = await fetch(`http://localhost:3500/time_schedule/${semId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      semester: semId,
+      schedule: timetable,
+    }),
+  });
+
+  if (!method.ok) {
+    throw new Error("Failed to save timetable");
+  }
+}
+
+export default function showTimetable({
+  params,
+}: {
+  params: { SemId: string };
+}) {
+  const [semid, setSemid] = useState<string>(params.SemId);
+  const [papers, setPapers] = useState([]);
+  const [timetable, setTimetable] = useState<any>({});
+
+  useEffect(() => {
+    if (semid) {
+      fetchPapers(semid)
+        .then((data) => setPapers(data))
+        .catch((error) => console.error(error));
+
+      fetchTimetable(semid)
+        .then((data) => setTimetable(data))
+        .catch((error) => console.error(error));
+    }
+  }, [semid]);
+
+  const handleSave = async () => {
+    try {
+      await saveTimetable(semid, timetable.schedule);
+      console.log("Timetable saved successfully!");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  function titleCase(str: string) {
+    return str
+      .toLowerCase()
+      .split(" ")
+      .map(function (word: string) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+  }
+
+  return (
+    <ScrollArea className="h-full">
+      <div className="container mx-auto py-10">
+        <Heading title={`Time Table`} description={``} />
+        <br />
+        <Separator />
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Day</TableHead>
+              <TableHead>1</TableHead>
+              <TableHead>2</TableHead>
+              <TableHead>3</TableHead>
+              <TableHead>4</TableHead>
+              <TableHead>5</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {["monday", "tuesday", "wednesday", "thursday", "friday"].map(
+              (day) => (
+                <TableRow key={day}>
+                  <TableHead>{titleCase(day)}</TableHead>
+                  {timetable.schedule &&
+                    timetable.schedule[day] &&
+                    timetable.schedule[day].map(
+                      (
+                        period: {
+                          paper:
+                            | string
+                            | number
+                            | boolean
+                            | ReactElement<
+                                any,
+                                string | JSXElementConstructor<any>
+                              >
+                            | Iterable<ReactNode>
+                            | ReactPortal
+                            | PromiseLikeOfReactNode
+                            | null
+                            | undefined;
+                          teacher: {
+                            name:
+                              | string
+                              | number
+                              | boolean
+                              | ReactElement<
+                                  any,
+                                  string | JSXElementConstructor<any>
+                                >
+                              | Iterable<ReactNode>
+                              | ReactPortal
+                              | PromiseLikeOfReactNode
+                              | null
+                              | undefined;
+                          };
+                        },
+                        index: Key | null | undefined
+                      ) => (
+                        <TableCell key={index}>
+                          <span className="text-l font-bold tracking-tight">
+                            {period.paper}
+                          </span>
+                          <br />
+                          <span className="text-sm text-muted-foreground">
+                            {period.teacher.name}
+                          </span>
+                        </TableCell>
+                      )
+                    )}
+                </TableRow>
+              )
+            )}
+          </TableBody>
+        </Table>
+        <br />
+      </div>
+    </ScrollArea>
+  );
+}
--- src/app/teacher/dashboard/timetable/page.tsx
+++ src/app/teacher/dashboard/timetable/page.tsx
@@ -1,14 +1,243 @@
 "use client";
-import Cookies from "js-cookie";
+import BreadCrumb from "@/components/breadcrumb";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import React, { useState } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
-export default function Page() {
-  const teacherId = Cookies.get("teacherId");
+const breadcrumbItems = [
+  { title: "Dashboard", link: "/teacher/dashboard/" },
+  { title: "Time Table", link: "/teacher/dashboard/timetable" },
+];
+
+const formSchema = z.object({
+  department: z.string(),
+  semnum: z.string(),
+});
+
+export default function CreateStudent() {
+  const [departments, setDepartments] = React.useState<any[]>([]);
+  const [selectedDepartment, setSelectedDepartment] = React.useState<
+    string | null
+  >(null);
+  const [SemId, setSemId] = useState<string | null>(null);
+  const [timeSchedules, setTimeSchedules] = useState<any[]>([]);
+  const router = useRouter();
+  const { toast } = useToast();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      department: "",
+      semnum: "",
+    },
+  });
+
+  React.useEffect(() => {
+    const fetchDepartments = async () => {
+      try {
+        const response = await fetch("http://localhost:3500/department");
+        if (response.ok) {
+          const data = await response.json();
+          setDepartments(data);
+        } else {
+          console.error("Failed to fetch departments");
+        }
+      } catch (error) {
+        console.error("Error during department fetch", error);
+      }
+    };
+
+    fetchDepartments();
+  }, []);
+
+  // Custom function to handle value change
+  const handleDepartmentChange = (value: string) => {
+    form.setValue("department", value);
+    setSelectedDepartment(value);
+    form.setValue("semnum", ""); // Reset semester field when department changes
+  };
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const { department, semnum } = values;
+    try {
+      const response = await fetch(
+        `http://localhost:3500/semester/dept/${department}/${semnum}`,
+        {
+          method: "GET",
+        }
+      );
+      console.log(response);
+
+      if (response.ok) {
+        const responseData = await response.json();
+
+        // Assuming the response data has an "_id" property
+        const semIdFromResponse = responseData?._id;
+
+        if (semIdFromResponse) {
+          setSemId(semIdFromResponse);
+          +console.log("fetched semid : " + semIdFromResponse);
+          router.push(`/teacher/dashboard/timetable/${semIdFromResponse}`);
+        } else {
+          console.error("Response data does not contain _id");
+        }
+      } else {
+        toast({
+          variant: "destructive",
+          description: "Failed to fetch sem id.",
+        });
+        console.error("Fetching failed");
+      }
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        description: "Error during fetch.",
+      });
+      console.error("Error during fetch", error);
+    }
+  }
+
+  //   const timeSchedulesResponse = await fetch(
+  //     `http://localhost:3500/time_schedule/${SemId}`,
+  //     {
+  //       method: "GET",
+  //     }
+  //   );
+  //   if (timeSchedulesResponse.ok) {
+  //     const timeSchedulesData = await timeSchedulesResponse.json();
+  //     setTimeSchedules(timeSchedulesData);
+  //     console.log("Fetched time schedules:", timeSchedulesData);
+  //   } else {
+  //     toast({
+  //       variant: "destructive",
+  //       description: "Failed to fetch time schedules.",
+  //     });
+  //     console.error("Fetching time schedules failed");
+  //   }
+
   return (
     <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">Time Table</h2>
+        <BreadCrumb items={breadcrumbItems} />
+        <div className="p-4 lg:p-8 h-full flex items-start">
+          <div className="mx-left flex w-full flex-col justify-start space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-left">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Manage Time Table
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Select the department and semester
+              </p>
+            </div>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-2 w-full"
+              >
+                <FormField
+                  control={form.control}
+                  name="department"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Department</FormLabel>
+                      <Select
+                        onValueChange={handleDepartmentChange} // Use setValue to update the form value
+                        value={form.getValues("department")} // Get the current value from the form
+                        defaultValue={form.getValues("department")}
+                      >
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue
+                              defaultValue={form.getValues("department")}
+                              placeholder="Select a department"
+                            />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {departments.map((department) => (
+                            <SelectItem
+                              key={department._id}
+                              value={department._id}
+                            >
+                              {department.deptname}{" "}
+                              {/* Use deptname from the API response */}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="semnum"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Semester</FormLabel>
+                      <Select
+                        onValueChange={(value) =>
+                          form.setValue("semnum", value)
+                        }
+                        value={form.getValues("semnum")}
+                        defaultValue={form.getValues("semnum")}
+                      >
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue
+                              defaultValue={form.getValues("semnum")}
+                              placeholder="Select a semester"
+                            />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {selectedDepartment &&
+                            Array.from(
+                              {
+                                length:
+                                  departments.find(
+                                    (dep) => dep._id === selectedDepartment
+                                  )?.semcount || 0,
+                              },
+                              (_, index) => (
+                                <SelectItem
+                                  key={index + 1}
+                                  value={(index + 1).toString()}
+                                >
+                                  Semester {index + 1}
+                                </SelectItem>
+                              )
+                            )}
+                        </SelectContent>
+                      </Select>
+                    </FormItem>
+                  )}
+                />
+                <br />
+                <Button type="submit" className="ml-auto">
+                  Fetch
+                </Button>
+              </form>
+            </Form>
+          </div>
         </div>
       </div>
     </ScrollArea>
